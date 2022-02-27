7bfd3e04bd0d32167c7661ee61cadaae393e68c4
Jinwoo Lee <starday456@kaist.ac.kr>
2022-02-27 15:28:17 +0900

    basic socket connection with server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, ChangeEventHandler, KeyboardEventHandler } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 const App = () => {
-  return <div className="App"></div>;
+  const [text, setText] = useState('');
+
+  const changeHandler: ChangeEventHandler = (e) => {
+    const value = (e.target as HTMLInputElement).value;
+
+    socket.emit('change', value);
+    setText(value);
+  };
+
+  const keyUpHandler: KeyboardEventHandler = (e) => {
+    const target = e.target as HTMLInputElement;
+    const position = target.selectionStart;
+  };
+
+  return (
+    <div className="App">
+      <textarea
+        onChange={changeHandler}
+        onKeyUp={keyUpHandler}
+        value={text}
+        cols={80}
+        rows={40}
+      ></textarea>
+    </div>
+  );
 };
 
 export default App;
