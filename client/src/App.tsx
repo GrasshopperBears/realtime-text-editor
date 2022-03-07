a0472e57b5a4359f92ffe7c4031c9e9a4820ba2c
Jinwoo Lee <starday456@kaist.ac.kr>
2022-03-08 00:31:39 +0900

    send delete event on deletion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,92 @@
-import React, { useState, ChangeEventHandler, KeyboardEventHandler } from 'react';
+import React, { useState, ChangeEventHandler, useEffect } from 'react';
 import io from 'socket.io-client';
+
+enum ChangeDataType {
+  INSERT,
+  DELETE,
+  REPLACE,
+}
+
+interface SocketChangeData {
+  source: string;
+  position: number;
+  type: ChangeDataType;
+  text?: string;
+  length?: number;
+}
 
 const socket = io('http://localhost:4000');
 
+const EVENT_TYPE_DELETE = ['deleteContentBackward', 'deleteContentForward', 'deleteByCut'];
+const EVENT_TYPE_UPDATE = ['insertText', 'insertFromPaste', 'historyUndo', 'historyRedo'];
+
 const App = () => {
-  const [text, setText] = useState('');
+  const [textareaText, setTextareaText] = useState('');
 
-  const changeHandler: ChangeEventHandler = (e) => {
-    const value = (e.target as HTMLInputElement).value;
+  useEffect(() => {
+    socket.on('change', socketChangeHandler);
+    return () => {
+      socket.removeAllListeners('change');
+    };
+  }, [textareaText]);
 
-    socket.emit('change', value);
-    setText(value);
+  const socketChangeHandler = (data: SocketChangeData) => {
+    const { source, text, position: newTextPosition, length = 0, type } = data;
+    if (source === socket.id) return;
+
+    if (type === ChangeDataType.DELETE) {
+      setTextareaText(
+        textareaText.slice(0, newTextPosition) + textareaText.slice(newTextPosition + length),
+      );
+    }
   };
 
-  const keyUpHandler: KeyboardEventHandler = (e) => {
+  const getChangedData = (
+    eventType: string,
+    position: number,
+    newText: string,
+  ): SocketChangeData | null => {
+    if (EVENT_TYPE_DELETE.includes(eventType))
+      return {
+        source: socket.id,
+        type: ChangeDataType.DELETE,
+        position,
+        length: textareaText.length - newText.length,
+      };
+
+    return null;
+  };
+
+  const changeHandler: ChangeEventHandler = (e) => {
     const target = e.target as HTMLInputElement;
-    const position = target.selectionStart;
+    const value = target.value;
+    const currentPosition = target.selectionStart ?? 0;
+    const eventType: string = (e.nativeEvent as any).inputType;
+
+    const changedData = getChangedData(eventType, currentPosition, value);
+    console.log(changedData);
+    /**
+     * e.navtiveEvent.inputType 종류 (string)
+     *
+     * 삭제
+     * deleteContentBackward (백스페이스)
+     * deleteContentForward (delete)
+     * deleteByCut
+     *
+     * 수정
+     * insertText
+     * insertFromPaste
+     * historyUndo
+     * historyRedo
+     */
+
+    socket.emit('change', changedData);
+    setTextareaText(value);
   };
 
   return (
     <div className="App">
-      <textarea
-        onChange={changeHandler}
-        onKeyUp={keyUpHandler}
-        value={text}
-        cols={80}
-        rows={40}
-      ></textarea>
+      <textarea onChange={changeHandler} value={textareaText} cols={80} rows={40}></textarea>
     </div>
   );
 };
